9f6866fff55a27ee178af63e8453b2bc5829a564
vanefeut <fvanessatherance@yahoo.fr>
2022-06-19 13:35:35 -0400

    change2

---
+++ jenkins2.sh
@@ -0,0 +1,25 @@
+ #Author : vanessa feutsa
+    #Date : 19-jun-2022
+
+## ----------Install jenkins on centos 7 server  -----------------
+
+#Step 1: Install Java
+yum install java-1.8.0-openjdk-devel
+
+#Step 2: Enable the Jenkins repository
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+
+if
+  [ $! eq 1]
+then
+yum install wget -y
+fi
+
+#Now, disable key check on the repo, run:
+sed -i 's/gpgcheck=1/gpgcheck=0/g' /etc/yum.repos.d/jenkins.repo
+
+#Step 3: Install the latest stable version of Jenkins
+yum install jenkins
+systemctl start jenkins
+systemctl status jenkins
+systemctl enable jenkins
